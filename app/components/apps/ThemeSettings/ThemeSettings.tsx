054be647bd3e367e2ac855b93a803fe1ac022e59
Teo Fumagalli <teo.fumagalli@aerolab.co>
2023-06-29 11:34:02 -0300

    select component

--- app/components/apps/ThemeSettings/ThemeSettings.tsx
+++ app/components/apps/ThemeSettings/ThemeSettings.tsx
@@ -1,25 +1,36 @@
 import useSystemStore from '~/stores/system';
 import { themes } from './types';
-import Button from '~/components/ui/Button';
+import { Select, SelectItem } from '~/components/ui/Select';
 
 export default function ThemeSettings() {
   const { theme, updateTheme } = useSystemStore();
 
+  const selectTheme = (value: string) => {
+    const newTheme = themes.find((t) => t.cssClass === value);
+    if (newTheme) updateTheme(newTheme);
+  };
+
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col gap-1.5 p-1">
       <div className="grid place-items-center w-80 h-60 bevel-content">
         TODO: Preview
       </div>
 
-      {themes.map((t) => (
-        <Button
-          key={t.cssClass}
-          disabled={t.cssClass === theme.cssClass}
-          onClick={() => updateTheme(t)}
+      <div className="flex flex-row items-baseline gap-2">
+        <span className="w-20">Color scheme</span>
+
+        <Select
+          value={theme.cssClass}
+          onValueChange={selectTheme}
+          triggerProps={{ className: 'flex-1' }}
         >
-          <span>{t.name}</span>
-        </Button>
-      ))}
+          {themes.map((t) => (
+            <SelectItem key={t.cssClass} value={t.cssClass}>
+              {t.name}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
     </div>
   );
 }
---
+++ app/components/ui/Select.tsx
@@ -0,0 +1,95 @@
+import * as SelectPrimitive from '@radix-ui/react-select';
+import cn from 'classnames';
+import { forwardRef } from 'react';
+import ArrowDown from './icons/ArrowDown';
+import Check from './icons/Check';
+
+type SelectProps = {
+  placeholder?: string;
+  triggerProps?: React.ComponentProps<typeof SelectPrimitive.Trigger>;
+  contentProps?: React.ComponentProps<typeof SelectPrimitive.Content>;
+  renderValue?: React.ReactNode;
+} & React.ComponentProps<typeof SelectPrimitive.Root>;
+
+export const Select = forwardRef<HTMLButtonElement, SelectProps>(
+  function Select(
+    {
+      placeholder,
+      triggerProps,
+      contentProps,
+      renderValue,
+      children,
+      ...props
+    },
+    ref,
+  ) {
+    return (
+      <SelectPrimitive.Root {...props}>
+        <SelectPrimitive.Trigger
+          ref={ref}
+          className={cn(
+            'flex flex-row items-stretch p-0.5',
+            'whitespace-nowrap overflow-hidden text-ellipsis',
+            'bg-default bevel-content outline-none group',
+            triggerProps?.className,
+          )}
+        >
+          <div className="m-px py-px px-0.5 flex-1 text-start group-focus:bg-selection group-focus:text-selection">
+            <SelectPrimitive.Value
+              placeholder={placeholder ?? 'Select an option'}
+            >
+              {renderValue}
+            </SelectPrimitive.Value>
+          </div>
+
+          <SelectPrimitive.Icon className="block button button-normal py-1">
+            <ArrowDown />
+          </SelectPrimitive.Icon>
+        </SelectPrimitive.Trigger>
+
+        <SelectPrimitive.Portal>
+          <SelectPrimitive.Content
+            position="popper"
+            side="bottom"
+            {...contentProps}
+            className={cn(
+              'bg-default border border-default z-2000',
+              'w-[var(--radix-select-trigger-width)]',
+              contentProps?.className,
+            )}
+          >
+            <SelectPrimitive.ScrollUpButton>
+              <ArrowDown className="rotate-180" />
+            </SelectPrimitive.ScrollUpButton>
+            <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
+            <SelectPrimitive.ScrollDownButton>
+              <ArrowDown />
+            </SelectPrimitive.ScrollDownButton>
+          </SelectPrimitive.Content>
+        </SelectPrimitive.Portal>
+      </SelectPrimitive.Root>
+    );
+  },
+);
+
+type ItemProps = React.ComponentProps<typeof SelectPrimitive.Item>;
+
+export const SelectItem = forwardRef<HTMLDivElement, ItemProps>(function Item(
+  { value, className, children, ...props },
+  ref,
+) {
+  return (
+    <SelectPrimitive.Item
+      className={cn(
+        'px-1 outline-none',
+        'data-[highlighted]:bg-selection data-[highlighted]:text-selection',
+        className,
+      )}
+      value={value}
+      ref={ref}
+      {...props}
+    >
+      <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+    </SelectPrimitive.Item>
+  );
+});
