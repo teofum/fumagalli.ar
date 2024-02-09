a67ae0865c2078a52f5ae838318c86161f3105ae
Teo Fumagalli <teo.fumagalli@aerolab.co>
2024-02-09 00:49:07 -0300

    chore: rm unused code

--- app/content/types.ts
+++
@@ -1,8 +0,0 @@
-import type { AnyFile } from '~/schemas/file';
-import type { Folder as FolderType } from '~/schemas/folder';
-
-/**
- * Filesystem object types
- */
-export type Folder = FolderType;
-export type FSObject = Folder | AnyFile;
