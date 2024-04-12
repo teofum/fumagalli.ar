671d53b0962e97dc375d34be438efa1a5a5ac063
Teo Fumagalli <teo.fum@gmail.com>
2024-04-12 10:04:12 -0300

    fix: sanity query for safeimage api endpoint

--- app/routes/api.safeimage.ts
+++ app/routes/api.safeimage.ts
@@ -1,6 +1,5 @@
 import { fetch, type LoaderArgs } from '@remix-run/node';
-import type { useLoaderData } from '@remix-run/react';
-import fileSchema from '~/schemas/file';
+import { imageFileSchema } from '~/schemas/file';
 import { sanityImage } from '~/utils/sanity.image';
 import { sanityClient } from '~/utils/sanity.server';
 
@@ -10,6 +9,9 @@ const fileQuery = (id: string) => `
   content {
     ...,
   },
+  'size': content.asset->size,
+  'lqip': content.asset->metadata.lqip,
+  'dimensions': content.asset->metadata.dimensions,
 }`;
 
 // Loads an image file through the server, preventing canvases from becoming tainted
@@ -17,12 +19,10 @@ export async function loader({ request }: LoaderArgs) {
   const url = new URL(request.url);
   const fileId = url.searchParams.get('id') ?? '';
 
-  const data = await fileSchema
+  const data = await imageFileSchema
     .promise()
     .parse(sanityClient.fetch(fileQuery(fileId)));
 
   const imageUrl = sanityImage(data.content).width(2000).url();
   return fetch(imageUrl);
 }
-
-export type SudokuPuzzle = ReturnType<typeof useLoaderData<typeof loader>>;
