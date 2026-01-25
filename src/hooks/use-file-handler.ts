07fef38f959baf1f237228472dc10cf2e3c5fc75
Teo Fumagalli <teo.fum@gmail.com>
2026-01-25 15:28:01 +0100

    fix file handler hook type errors

--- src/hooks/use-file-handler.ts
+++ src/hooks/use-file-handler.ts
@@ -1,14 +1,17 @@
-import { dosEmu } from "@/components/apps/DOSEmu";
-import { DOS_GAMES } from "@/components/apps/DOSEmu/types";
-import { preview } from "@/components/apps/Preview";
-import { previewSupportedFileTypes } from "@/components/apps/Preview/types";
-import { getApp } from "@/components/apps/renderApp";
-import type { ItemStub } from "@/schemas/folder";
-import useDesktopStore from "@/stores/desktop";
-import useSystemStore from "@/stores/system";
+import { dosEmu } from '@/components/apps/DOSEmu';
+import { DOS_GAMES } from '@/components/apps/DOSEmu/types';
+import { preview } from '@/components/apps/Preview';
+import { previewSupportedFileTypes } from '@/components/apps/Preview/types';
+import { getApp } from '@/components/apps/renderApp';
+import { AnyFile } from '@/schemas/file';
+import type { ItemStub } from '@/schemas/folder';
+import useDesktopStore from '@/stores/desktop';
+import useSystemStore from '@/stores/system';
 
 function isPreviewable(file: ItemStub) {
-  return previewSupportedFileTypes.includes(file._type as any);
+  return previewSupportedFileTypes.includes(
+    file._type as (typeof previewSupportedFileTypes)[number],
+  );
 }
 
 export default function useFileHandler() {
@@ -21,26 +24,26 @@ export default function useFileHandler() {
     if (isPreviewable(stub)) {
       launch(preview({ fileStub: stub }));
       handled = true;
-    } else if (stub._type === "fileApp") {
-      const app = getApp(stub.name.split(".")[0]);
+    } else if (stub._type === 'fileApp') {
+      const app = getApp(stub.name.split('.')[0]);
       if (app) {
         launch(app);
         handled = true;
       }
-    } else if (stub._type === "fileDos") {
+    } else if (stub._type === 'fileDos') {
       launch(
         dosEmu(
           DOS_GAMES.find(
             (game) =>
-              game.title.toLowerCase().replace(" ", "_") ===
-              stub.name.split(".")[0],
+              game.title.toLowerCase().replace(' ', '_') ===
+              stub.name.split('.')[0],
           ),
         ),
       );
     }
 
     if (handled) {
-      saveFileToHistory({ time: Date.now(), item: stub });
+      saveFileToHistory({ time: Date.now(), item: stub as AnyFile });
     }
 
     return handled;
