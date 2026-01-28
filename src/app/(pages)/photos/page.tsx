08b44f6db6b401fcb60e2ddc7cd5fd09637ab736
Teo Fumagalli <teo.fum@gmail.com>
2026-01-28 01:21:28 +0100

    move to main photos route

--- src/app/(pages)/photos/page.tsx
+++ src/app/(pages)/photos/page.tsx
@@ -1,15 +1,46 @@
 import Link from 'next/link';
 
-import Collapsible from '@/components/pages/Collapsible';
-import { PHOTO_CATEGORY_QUERY } from '@/queries/queries';
-import { photoCategorySchema } from '@/schemas/photos';
+import { PHOTOS_QUERY } from '@/queries/queries';
+import { Photo, photoSchema } from '@/schemas/photos';
 import { sanityImage } from '@/utils/sanity.image';
 import { sanityClient } from '@/utils/sanity.server';
+import { SearchParams, ServerComponentProps } from '@/utils/types';
 
-export default async function Photos() {
-  const data = photoCategorySchema
+import { fetchTags } from './use-tags';
+import Filters from './filters';
+import Collapsible from '@/components/pages/Collapsible';
+
+const DEFAULT_SORT_MODE: Record<string, 'desc' | 'asc'> = {
+  date: 'desc',
+  filename: 'asc',
+};
+
+const sortFn: Record<string, (a: Photo, b: Photo) => number> = {
+  date: (a, b) =>
+    a.metadata.exif.dateTime.getTime() - b.metadata.exif.dateTime.getTime(),
+  filename: (a, b) => a.originalFilename.localeCompare(b.originalFilename),
+};
+
+function getSortFn(sort: SearchParams[string]) {
+  const [sortBy, sortMode] =
+    typeof sort === 'string' ? sort.split(',') : ['date', 'desc'];
+
+  const fn = sortFn[sortBy];
+  const mode = sortMode ?? DEFAULT_SORT_MODE[sortBy] ?? 'asc';
+
+  return mode === 'desc' ? (a: Photo, b: Photo) => fn(b, a) : fn;
+}
+
+export default async function Photos({ searchParams }: ServerComponentProps) {
+  const { sort, ...filters } = await searchParams;
+
+  const photos = photoSchema
     .array()
-    .parse(await sanityClient.fetch(PHOTO_CATEGORY_QUERY));
+    .parse(await sanityClient.fetch(PHOTOS_QUERY(filters)));
+
+  photos.sort(getSortFn(sort));
+
+  const tags = await fetchTags();
 
   return (
     <div className="max-w-3xl mx-auto">
@@ -17,45 +48,38 @@ export default async function Photos() {
         Photography
       </h1>
 
-      <p className="my-4">
-        Some of my work as a hobby photographer. Click any photo to view full
-        size.
+      <p className="mb-4">
+        My work as an amateur photographer. Click on any image for a detail
+        view.
       </p>
 
-      <div className="grid grid-cols-1">
-        {data.map((category) => (
-          <Collapsible key={category._id} title={category.title}>
-            <div className="grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] group-data-[state='open']/content:pt-3 gap-3">
-              {category.collections.map((collection) => (
-                <Link
-                  key={collection._id}
-                  href={`photos/${collection.slug}`}
-                  className="block relative overflow-hidden group"
-                >
-                  <img
-                    className="absolute inset-0 w-full h-full object-cover [image-rendering:auto]"
-                    alt=""
-                    src={collection.thumbnail.lqip ?? undefined}
-                  />
+      <Collapsible
+        title="Filters"
+        className="mb-6 sticky top-0 z-100 bg-default backdrop-blur-lg"
+      >
+        <Filters tags={tags} defaultValues={filters} />
+      </Collapsible>
 
-                  <img
-                    className="relative w-full aspect-3/2 group-hover:scale-[1.05] transition-transform duration-200"
-                    alt=""
-                    src={sanityImage(collection.thumbnail.content)
-                      .width(524)
-                      .dpr(2)
-                      .quality(80)
-                      .url()}
-                    loading="lazy"
-                  />
+      <div className="grid grid-cols-2 gap-2">
+        {photos.map((photo) => (
+          <Link
+            key={photo._id}
+            href={`photos/${photo._id}`}
+            className="block relative overflow-hidden group"
+          >
+            <img
+              className="absolute inset-0 w-full h-full object-cover [image-rendering:auto]"
+              alt=""
+              src={photo.metadata.lqip ?? undefined}
+            />
 
-                  <div className="absolute left-0 bottom-0 w-full bg-default/20 pixelate-bg py-3 px-6 text-white">
-                    {collection.title}
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </Collapsible>
+            <img
+              className="relative w-full aspect-3/2 group-hover:scale-[1.05] transition-transform duration-200 object-cover"
+              alt=""
+              src={sanityImage(photo._id).width(524).dpr(2).quality(80).url()}
+              loading="lazy"
+            />
+          </Link>
         ))}
       </div>
 
