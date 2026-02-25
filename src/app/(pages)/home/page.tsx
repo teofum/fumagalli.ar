17ab359b3584ce4a72da903170923ae995004c46
Teo Fumagalli <teo.fum@gmail.com>
2026-02-26 00:52:36 +0100

    webring shtuff

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -10,6 +10,7 @@ import { sanityClient } from '@/utils/sanity.server';
 import fetchArticles from '../articles/fetch-articles';
 import { PhotoThumbnail } from '../photos/photo-thumbnail';
 import ArticleItem from '../article-item';
+import Webring from '@/components/ui/Webring';
 
 const MS_PER_DAY = 1000 * 60 * 60 * 24;
 
@@ -86,111 +87,16 @@ export default async function Home() {
           <ChevronRight size={20} />
         </Link>
 
-        {/*<div className="bg-surface bevel-window p-1 flex flex-col font-sans text-base">
-          <div className="select-none flex flex-row items-center gap-2 px-0.5 py-px mb-0.5">
-            <Image
-              width={16}
-              height={16}
-              unoptimized
-              src={`/fs/Applications/intro/icon_16.png`}
-              alt=""
-            />
-
-            <div className="flex-1 h-1.5 border-t border-b border-light" />
-
-            <span className="text-title bold">About Me</span>
-
-            <div className="flex-1 h-1.5 border-t border-b border-light" />
-
-            <Button>
-              <Close />
-            </Button>
-          </div>
-
-          <div className="bg-default bevel-content p-0.5 flex flex-col sm:flex-row gap-2">
-            <div className="flex flex-col flex-1 p-4 gap-2">
-              <p>Hi there! My name is</p>
-              <h1 className="font-display text-4xl">
-                <span className="tracking-[-3px]">T</span>eo{' '}
-                <span className="tracking-[-2px]">F</span>umaga
-                <span className="tracking-[-9px] -ml-1.5">lli</span>
-              </h1>
-              <p>
-                I&apos;m a programmer, UI designer and amateur photographer
-                based in Buenos Aires, Argentina. I do a bit of everything, but
-                my professional background is in web development and my primary
-                interest in rendering and graphics programming.
-              </p>
-
-              <p>
-                Welcome to my site! Click on the Start button or one of the
-                desktop shortcuts to begin, or check out some of my stuff:
-              </p>
-
-              <div className="flex flex-row gap-1 my-2">
-                <LinkButton href="/articles" className="py-1 px-2 w-28">
-                  <div className="flex flex-row items-center gap-2">
-                    <Image
-                      width={32}
-                      height={32}
-                      unoptimized
-                      src={`${resources}/articles.png`}
-                      alt=""
-                    />
-                    Articles
-                  </div>
-                </LinkButton>
-                <LinkButton href="/photos" className="py-1 px-2 w-28">
-                  <div className="flex flex-row items-center gap-2">
-                    <Image
-                      width={32}
-                      height={32}
-                      unoptimized
-                      src={`${resources}/photos.png`}
-                      alt=""
-                    />
-                    Photos
-                  </div>
-                </LinkButton>
-                <LinkButton href="/projects" className="py-1 px-2 w-28">
-                  <div className="flex flex-row items-center gap-2">
-                    <Image
-                      width={32}
-                      height={32}
-                      unoptimized
-                      src={`${resources}/projects.png`}
-                      alt=""
-                    />
-                    Projects
-                  </div>
-                </LinkButton>
-              </div>
-
-              <p className="mt-auto">
-                Let&apos;s build awesome things together!{' '}
-                <RetroLink href="mailto:teo.fum@outlook.com">
-                  Get in touch
-                </RetroLink>
-              </p>
-
-              <p>This site is part of the following webrings</p>
-              <Webring
-                indexUrl="https://graphics-programming.org/webring"
-                baseUrl="https://graphics-programming.org/webring/frogs/bluescreen"
-                iconUrl="/assets/misc/froge.webp"
-              />
-            </div>
-
-            <Image
-              width={199}
-              height={400}
-              unoptimized
-              src={`${resources}/me2.png`}
-              alt="me"
-              className="self-end h-100 [image-rendering:pixelated]"
-            />
-          </div>
-        </div>*/}
+        <div className="mt-8 flex flex-col items-center gap-2">
+          <p className="text-content-sm">
+            This site is part of the following webrings:
+          </p>
+          <Webring
+            indexUrl="https://graphics-programming.org/webring"
+            baseUrl="https://graphics-programming.org/webring/frogs/bluescreen"
+            iconUrl="/assets/misc/froge.webp"
+          />
+        </div>
       </div>
     </main>
   );
