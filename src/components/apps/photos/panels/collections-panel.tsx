ca7d3ee0d2ba9fefe9c5a48911c09a78285fc566
Teo Fumagalli <teo.fum@gmail.com>
2026-03-05 13:18:45 +0100

    add photo count to collections

--- src/components/apps/photos/panels/collections-panel.tsx
+++ src/components/apps/photos/panels/collections-panel.tsx
@@ -4,14 +4,59 @@ import { useAppState } from '@/components/desktop/Window/context';
 import Divider from '@/components/ui/Divider';
 import ScrollContainer from '@/components/ui/ScrollContainer';
 import useFetch from '@/hooks/use-fetch';
-import { type PhotoCategory } from '@/schemas/photos';
+import type { PhotoCategory, PhotoCollectionBase } from '@/schemas/photos';
+
+type PhotoCollectionProps = {
+  collection: PhotoCollectionBase;
+};
+
+function PhotoCollection({ collection }: PhotoCollectionProps) {
+  const [state, update] = useAppState('photos');
+
+  const selected = collection.slug === state.collection;
+  const select = () => update({ collection: collection.slug });
+
+  const { load, data: count } = useFetch<number>();
+  useEffect(() => {
+    load(`/api/photos/${collection.slug}/count`);
+  }, [load, collection.slug]);
+
+  return (
+    <li key={collection._id} className="pl-2 py-0.5">
+      <button
+        className="button w-full flex flex-row items-start relative z-1 group"
+        onClick={select}
+      >
+        <span className="relative min-w-4 outline-none!">
+          <img
+            className="w-4 h-4"
+            src={`/assets/icons/directory_${selected ? 'open' : 'closed'}.png`}
+            alt=""
+          />
+          <span
+            className="absolute inset-0 bg-selection/50 hidden group-focus:inline"
+            style={{
+              WebkitMaskImage: `url('/assets/icons/directory_${selected ? 'open' : 'closed'}.png')`,
+            }}
+          />
+        </span>
+        <span className="self-center ml-1 group-focus:bg-selection group-focus:text-selection">
+          {collection.title}
+        </span>
+        <span className="self-center ml-auto mr-1 text-light outline-none!">
+          {count ?? '--'}
+        </span>
+      </button>
+    </li>
+  );
+}
 
 type PhotoCategoryProps = {
   category: PhotoCategory;
 };
 
 function PhotoCategory({ category }: PhotoCategoryProps) {
-  const [state, update] = useAppState('photos');
+  const [state] = useAppState('photos');
 
   const [expanded, setExpanded] = useState(
     category.collections.some((c) => c.slug === state.collection),
@@ -35,36 +80,9 @@ function PhotoCategory({ category }: PhotoCategoryProps) {
       {expanded ? (
         <ul>
           <Divider className="my-1" />
-          {category.collections.map((collection) => {
-            const selected = collection.slug === state.collection;
-            const select = () => update({ collection: collection.slug });
-
-            return (
-              <li key={collection._id} className="pl-2 py-0.5">
-                <button
-                  className="button w-full flex flex-row items-start relative z-1 group"
-                  onClick={select}
-                >
-                  <span className="relative min-w-4 outline-none!">
-                    <img
-                      className="w-4 h-4"
-                      src={`/assets/icons/directory_${selected ? 'open' : 'closed'}.png`}
-                      alt=""
-                    />
-                    <span
-                      className="absolute inset-0 bg-selection/50 hidden group-focus:inline"
-                      style={{
-                        WebkitMaskImage: `url('/assets/icons/directory_${selected ? 'open' : 'closed'}.png')`,
-                      }}
-                    />
-                  </span>
-                  <span className="self-center ml-1 group-focus:bg-selection group-focus:text-selection">
-                    {collection.title}
-                  </span>
-                </button>
-              </li>
-            );
-          })}
+          {category.collections.map((collection) => (
+            <PhotoCollection key={collection.slug} collection={collection} />
+          ))}
         </ul>
       ) : null}
     </li>
@@ -73,16 +91,19 @@ function PhotoCategory({ category }: PhotoCategoryProps) {
 
 export default function CollectionsPanel() {
   const { load, data: categories } = useFetch<PhotoCategory[]>();
+  const { load: loadCount, data: count } = useFetch<number>();
 
   useEffect(() => {
     load('/api/photo_categories');
-  }, [load]);
+    loadCount('/api/photos/count');
+  }, [load, loadCount]);
 
   const [state, update] = useAppState('photos');
   const selected = state.collection === null;
 
   return (
     <div className="w-52 min-w-52 flex flex-col gap-0.5">
+      <div className="bold px-2 select-none">Catalog</div>
       <div className="bevel-inset p-0.5">
         <div className="bevel bg-surface p-1">
           <button
@@ -104,6 +125,9 @@ export default function CollectionsPanel() {
             </span>
             <span className="self-center ml-1 group-focus:bg-selection group-focus:text-selection">
               All photos
+            </span>
+            <span className="self-center ml-auto mr-1 text-light outline-none!">
+              {count ?? '--'}
             </span>
           </button>
         </div>
